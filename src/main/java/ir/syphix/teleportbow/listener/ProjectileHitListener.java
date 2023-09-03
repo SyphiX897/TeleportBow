85315e97bc70012843476ebb2f5b261b91bfc119
SyphiX897 <nimajanbaz18@gmail.com>
2023-09-03 16:22:55 +0330

    Added lore for teleportbow and arrow

--- src/main/java/ir/syphix/teleportbow/listener/ProjectileHitListener.java
+++ src/main/java/ir/syphix/teleportbow/listener/ProjectileHitListener.java
@@ -19,26 +19,29 @@ public class ProjectileHitListener implements Listener {
 
     @EventHandler
     public void onHit(ProjectileHitEvent event) {
-        if (event.getEntity().getShooter() instanceof Player) {
-            Player player = (Player) event.getEntity().getShooter();
-            if (!(event.getEntity() instanceof Arrow)) return;
-            Arrow arrow = (Arrow) event.getEntity();
+        if (event.getEntity().getShooter() instanceof Player player) {
+            if (!(event.getEntity() instanceof Arrow arrow)) return;
             PersistentDataContainer arrowData = arrow.getPersistentDataContainer();
             if (arrowData.has(Items.ARROW_ENTITY_KEY)) {
                 if (player.hasPermission("teleportbow.use")) {
                     if (event.getHitEntity() != null) {
-                        Location hitEntityLocation = event.getEntity().getLocation();
-                        player.teleport(entityLocation(player, hitEntityLocation));
-                        arrowGiver(player, event.getEntity());
+                        if (config.getBoolean("teleport-to-entity")) {
+                            Location hitEntityLocation = event.getEntity().getLocation();
+                            player.teleport(entityLocation(player, hitEntityLocation));
+                            arrowGiver(player, event.getEntity());
+                            return;
+                        }
                     }
                     if (event.getHitBlock() != null) {
-                        Location arrowLocation = event.getEntity().getLocation();
-                        player.setInvulnerable(true);
+                        if (config.getBoolean("teleport-to-block")) {
+                            Location arrowLocation = event.getEntity().getLocation();
+                            player.setInvulnerable(true);
 
-                        player.teleport(arrowLocation(player, arrowLocation));
+                            player.teleport(arrowLocation(player, arrowLocation));
 
-                        player.setInvulnerable(false);
-                        arrowGiver(player, event.getEntity());
+                            player.setInvulnerable(false);
+                            arrowGiver(player, event.getEntity());
+                        }
                     }
                 }
             }
