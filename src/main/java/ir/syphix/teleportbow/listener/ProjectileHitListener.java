66dff507184d41147870451e92bd54a22465c01c
SyphiX897 <nimajanbaz18@gmail.com>
2023-09-03 16:25:53 +0330

    Added some features to the configuration file

--- src/main/java/ir/syphix/teleportbow/listener/ProjectileHitListener.java
+++ src/main/java/ir/syphix/teleportbow/listener/ProjectileHitListener.java
@@ -19,8 +19,10 @@ public class ProjectileHitListener implements Listener {
 
     @EventHandler
     public void onHit(ProjectileHitEvent event) {
-        if (event.getEntity().getShooter() instanceof Player player) {
-            if (!(event.getEntity() instanceof Arrow arrow)) return;
+        if (event.getEntity().getShooter() instanceof Player) {
+            Player player = (Player) event.getEntity().getShooter();
+            if (!(event.getEntity() instanceof Arrow)) return;
+            Arrow arrow = (Arrow) event.getEntity();
             PersistentDataContainer arrowData = arrow.getPersistentDataContainer();
             if (arrowData.has(Items.ARROW_ENTITY_KEY)) {
                 if (player.hasPermission("teleportbow.use")) {
